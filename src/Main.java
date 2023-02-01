9f215f06f2fe28bc073af4fff4b2971dcf0ad29a
Alexandra <Kaverzina0331@mail.ru>
2023-02-01 19:23:18 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+/*Даны два Deque представляющие два неотрицательных целых числа.
+ Цифры хранятся в обратном порядке, и каждый из их узлов содержит одну цифру.
+Сложите два числа и верните сумму в виде связанного списка.
+
+public static void main(String[] args) {
+        Deque<Integer> d1 = new ArrayDeque<>(Arrays.asList(1,2,3));
+        Deque<Integer> d2 = new ArrayDeque<>(Arrays.asList(5,4,7));
+        // result [6,6,0,1]
+        }
+public Deque<Integer> sum(Deque<Integer> d1, Deque<Integer> d2) {
+        return new ArrayDeque<>();
+        }
+       } */
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Deque<Integer> d1 = new ArrayDeque<>(Arrays.asList(1, 2, 3));
+        Deque<Integer> d2 = new ArrayDeque<>(Arrays.asList(5, 4, 7));
+        Deque<Integer> res = sum(d1, d2);
+        System.out.println(res);
+    }
+
+    public static Deque<Integer> sum(Deque<Integer> d1, Deque<Integer> d2) {
+        int len = d1.size() < d2.size() ? d1.size() : d2.size();
+        Deque<Integer> d3 = new ArrayDeque<>();
+        while (len > 0){
+            int tmp = d1.pollFirst() + d2.pollFirst();
+            if (tmp > 9) {
+                d3.addLast(tmp % 10);
+                d3.addLast(1);
+            }
+            else d3.addLast(tmp);
+            len --;
+        }
+        return new ArrayDeque<>(d3);
+    }
+}
